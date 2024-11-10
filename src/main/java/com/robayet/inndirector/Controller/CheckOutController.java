382de1d0cc4a6b628bd1057f7b5ca8bf08079356
juwelxgithub <robayetferdous65@gmail.com>
2024-11-10 20:38:55 +0600

    Update checkout system
    -room numbers are now showing in chicebox

--- src/main/java/com/robayet/inndirector/Controller/CheckOutController.java
+++ src/main/java/com/robayet/inndirector/Controller/CheckOutController.java
@@ -1,9 +1,23 @@
 package com.robayet.inndirector.Controller;
 
 import com.robayet.inndirector.Main;
+import com.robayet.inndirector.model.RoomInfo;
+import com.robayet.inndirector.service.DateAndTimeService;
+import com.robayet.inndirector.service.RoomService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 
-public class CheckOutController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class CheckOutController implements Initializable {
     @FXML
     public void clickLogout(){
         Main.changeScene("login");
@@ -19,5 +33,35 @@ public class CheckOutController {
     @FXML
     public void clickCustomerInfo(){
         Main.changeScene("customerInfo");
+    }
+    @FXML
+    private ChoiceBox<Integer> roomCoiceBox;
+    @FXML
+    private Label dateTextField;
+    @FXML
+    void clickCheckOut(ActionEvent event) {
+
+    }
+    private ObservableList<Integer> integerObservableList;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        DateAndTimeService dateAndTimeService = new DateAndTimeService();
+        String date = dateAndTimeService.getCurrentDataAndTime();
+
+        dateTextField.setText(date);
+
+        //choice box
+        RoomService roomService = new RoomService();
+        List<RoomInfo> roomInfoList = roomService.list();
+
+        List<Integer> roomNumbers = new ArrayList<>();
+        for (RoomInfo room : roomInfoList){
+            roomNumbers.add(room.getNumber());
+        }
+
+        integerObservableList = FXCollections.observableArrayList();
+        integerObservableList.addAll(roomNumbers);
+        roomCoiceBox.setItems(integerObservableList);
+
     }
 }
